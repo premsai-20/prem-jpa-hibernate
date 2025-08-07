8823cd93fa8e09006ad537b0bac2c209786bea38
premsai-20 <sakamurisai252@gmail.com>
2025-08-07 10:26:42 +0530

    Initial commit

---
+++ src/main/java/com/premsai/springboot/learn_jap_and_hibernate/LearnJapAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.premsai.springboot.learn_jap_and_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJapAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJapAndHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/premsai/springboot/learn_jap_and_hibernate/LearnJapAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premsai.springboot.learn_jap_and_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJapAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
